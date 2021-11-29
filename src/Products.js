eb2e8ea7b15659451d18e34f9091becd35a83515
kevin knight <tgodkev@gmail.com>
2021-11-29 00:04:45 -0500

    cleaned up cards

--- src/Products.js
+++ src/Products.js
@@ -1,25 +1,30 @@
 const products = [
     {
+        id: 1,
         name: "Chips",
         img: "https://www.beyondthechickencoop.com/wp-content/uploads/2020/07/Homemade-Potato-Chips-1-of-1.jpg",
         price: "1.99"
     },
     {
+        id: 2,
         name: "Pizza",
         img: "https://media.istockphoto.com/photos/tasty-pepperoni-pizza-and-cooking-ingredients-tomatoes-basil-on-black-picture-id1083487948?k=20&m=1083487948&s=612x612&w=0&h=ROZ5t1K4Kjt5FQteVxTyzv_iqFcX8aqpl7YuA1Slm7w=",
         price: "8.99"
     },
     {
+        id: 3,
         name: "Gummy Bears",
         img: "https://media.candynation.com/catalog/product/cache/3cd2ce2b9f5add902f37baa77584be72/a/l/albanese_12_flavor.jpg", 
         price: "1.50"
     },
     {
+        id: 4,
         name: "Combos",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMvfPs9P6tlD1S-JBe25XP5LrcQNlKpDHgnQ&usqp=CAU", 
         price: "1.99"
     },
     {
+        id: 5,
         name: "Supreme Oreo",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd0GON_1NScLsnZ_DYMBPiTXOxqVGIpvIaIQ&usqp=CAU",
         price: "92,000"
--- src/components/App.js
+++ src/components/App.js
@@ -4,38 +4,25 @@ import Footer from './Footer'
 import products from '../Products';
 import Card from './Card';
 
+
+function createCard(product){
+  return(
+    <Card 
+      key={product.id}
+      name={product.name}
+      img={product.img}
+      price={product.price}
+    />
+  )
+
+}
+
+
 function App() {
   return (
     <div>
-    <div>
       <Header />
-      <Card 
-      name = {products[0].name}
-      img = {products[0].img}
-      price = {products[0].price}
-      />
-      <Card 
-      name = {products[1].name}
-      img = {products[1].img}
-      price = {products[1].price}
-      />
-      <Card 
-      name = {products[2].name}
-      img = {products[2].img}
-      price = {products[2].price}
-      />
-      <Card 
-      name = {products[3].name}
-      img = {products[3].img}
-      price = {products[3].price}
-      />
-      <Card 
-      name = {products[4].name}
-      img = {products[4].img}
-      price = {products[4].price}
-      />
-
-      </div>
+      {products.map(createCard)}
 
       <Footer />
     </div>
