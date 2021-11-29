cb1fc4072f1cbbf31beea6755e026f881f6e155c
kevin knight <tgodkev@gmail.com>
2021-11-28 23:30:52 -0500

    added modal, might not keep it .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Modal from "react-modal";
+
 
 function Header(){
+
+    let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = 'black';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
     return(
         <header>
             <h2>Snack Spot</h2>
+            <button onClick={openModal}>My Cart</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        className="customStyles"
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Checkout</h2>
+        <div>Youre Cart</div>
+        <form>
+          <button>Checkout</button>
+          <button>Empty cart</button>
+          <button onClick={closeModal}>close</button>
+
+        </form>
+      </Modal>
         </header>
     )
 }
